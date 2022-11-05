bdca80d6e27b89b0134c2ba4989ab0f22640507d
Richard Joyal <richardjoyal777@gmail.com>
2022-11-05 09:55:21 +0530

    Assignment 4 done

---
+++ ASSIGNMENTS/Team Member 2/ASSIGNMENT 4/Assignment_4_2/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import passportConfig from "./lib/passportConfig.js";
+import cors from "cors";
+import fs from "fs";
+
+import authRoutes from "./routes/authRoutes.js";
+import apiRoutes from "./routes/apiRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import downloadRoutes from "./routes/downloadRoutes.js";
+
+// MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/jobPortal", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+// initialising directories
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
+}
+
+const app = express();
+const port = 4444;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+// Routing
+app.use("/auth", authRoutes);
+app.use("/api",apiRoutes);
+app.use("/upload", uploadRoutes);
+app.use("/host", downloadRoutes);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
